d13bbced56495aa3f86dd8b144e9743cdce4ac55
Daniel Bradburn <d.bradburn@amsterdam.nl>
2022-07-27 11:56:42 +0200

    fix migration

--- src/datasets/blackspots/migrations/0022_spot_linestring_to_polygon.py
+++ src/datasets/blackspots/migrations/0022_spot_linestring_to_polygon.py
@@ -6,23 +6,28 @@ from django.contrib.gis.geos import Polygon
 
 
 def line_string_to_polygon(apps, schema_editor):
-    Spot = apps.get_model('Spot')
+    Spot = apps.get_model('blackspots', 'Spot')
     for spot in Spot.objects.all():
         if spot.polygoon is not None:
-            spot._polygoon = Polygon((*spot.polygoon, spot.polygoon[0]))
+            spot.polygoon = Polygon((*spot.wegvak, spot.wegvak[0]))
             spot.save()
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blackspots', '0020_alter_spot_point_nullable'),
+        ('blackspots', '0021_spot_point_nullable'),
     ]
 
     operations = [
+        migrations.RenameField(
+            model_name='spot',
+            old_name='polygoon',
+            new_name='wegvak',
+        ),
         migrations.AddField(
             model_name='spot',
-            name='_polygoon',
+            name='polygoon',
             field=django.contrib.gis.db.models.fields.PolygonField(blank=True, null=True, srid=4326),
         ),
         migrations.RunPython(line_string_to_polygon),
